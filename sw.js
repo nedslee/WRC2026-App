27dc6b8c51f6fbf9a100e8afb574dc5bfe71da58
nedslee <nedslee@gmail.com>
2026-08-19 09:52:10 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'wrc2026-release-v1-0-2';
+const CACHE = 'wrc2026-release-v1-0-4';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icons/icon-192-v102.png','./icons/icon-512-v102.png'];
 
 self.addEventListener('install', event => {
